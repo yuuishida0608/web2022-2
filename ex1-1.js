500578950a8761b097cdf32df70b331664315112
yuuishida0608 <zm19254@josai.ac.jp>
2022-02-04 18:16:06 +0000

    02/05

---
+++ ex1-1.js
@@ -0,0 +1,5 @@
+function imgchange1()
+ {document.getElementById('logo').src= './image/IMG_1946.jpeg';}
+ 
+ function imgchange2()
+ {document.getElementById('logo').src= './image/IMG_2668.jpeg';}
---
+++ ex6-3.js
@@ -0,0 +1,2 @@
+function sayhello71()
+ {alert('Hello, ' + document.getElementById('who71').value + '!');}
